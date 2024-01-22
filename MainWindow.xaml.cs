22ef6a5f97849f2290b1aa6771bd232083c02327
Marcin Bulanda <themarcin349@gmail.com>
2024-01-22 02:05:00 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+namespace wykresCw
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePlot_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (double.TryParse(txtA.Text, out double a) &&
+                    double.TryParse(txtB.Text, out double b) &&
+                    double.TryParse(txtC.Text, out double c))
+                {
+                    Window xd = new NoweOkno(a, b, c);
+                    xd.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Nieprawidłowe dane. Proszę wprowadzić poprawne wartości liczbowe dla A, B i C.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wystąpił błąd: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ NoweOkno.xaml.cs
@@ -0,0 +1,58 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace wykresCw
+{
+    public partial class NoweOkno : Window
+    {
+        public ChartValues<ObservablePoint> ChartValues { get; set; }
+
+        public NoweOkno(double a, double b, double c)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            ChartValues = new ChartValues<ObservablePoint>();
+            for (double x = -10; x <= 10; x += 0.1)
+            {
+                double y = a * x * x + b * x + c;
+                ChartValues.Add(new ObservablePoint(x, y));
+            }
+        }
+
+        private void SaveChartImage_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Pliki obrazów (*.png)|*.png",
+                DefaultExt = "png",
+                Title = "Zapisz wykres jako obraz"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                SaveAsPng(chart, saveFileDialog.FileName);
+                MessageBox.Show($"Wykres został zapisany jako: {saveFileDialog.FileName}", "Zapisano");
+            }
+        }
+
+        private static void SaveAsPng(FrameworkElement visual, string fileName)
+        {
+            var encoder = new PngBitmapEncoder();
+            var bitmap = new RenderTargetBitmap((int)visual.ActualWidth, (int)visual.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(visual);
+
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+        }
+    }
+}
